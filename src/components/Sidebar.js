c821a57f6979f8243488eacd3033d80aed10b12d
romanborynets0 <romanborynets@gmail.com>
2025-02-21 20:35:37 +0200

    Implement cart functionality and integrate with product and sidebar components(55.30)

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,11 @@ import { IoMdArrowForward } from 'react-icons/io';
 import { FiTrash2 } from 'react-icons/fi';
 import CartItem from '../components/CartItem';
 import { SidebarContext } from '../contexts/SidebarContext';
+import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
+  const {cart} = useContext(CartContext);
   return (<div className={`${
     isOpen ? 'right-0' : '-right-full'
     } w-full bg-white fixed top-0 h-full shadow-2x1 md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -17,6 +19,9 @@ const Sidebar = () => {
         <IoMdArrowForward className='text-2xl' />
       </div>
     </div>
+    <div>{cart.map((item) => {
+      return <CartItem>{item.title}</CartItem>
+    }) }</div>
     </div>
   );
 };
