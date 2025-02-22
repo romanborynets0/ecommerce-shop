f3ba43e465feb6761bb2487dc322d4d71bb53c98
romanborynets0 <romanborynets@gmail.com>
2025-02-22 22:28:05 +0200

    Enhance cart functionality: implement CartItem component, update Header for item count, and improve Sidebar with total and clear cart features

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
+import { parse } from 'postcss';
+import { CartContext } from '../contexts/CartContext';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext);
+
+  const { id, title, image, price, amount } = item;
+
+  return (
+    <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
+      <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+        {/* image */}
+        <Link to={`/products/${id}`}>
+          <img className='max-w-[80px]' src={image} alt={title} />
+        </Link>
+        <div className='w-full flex flex-col'>
+        {/* title & remove icon */}
+          <div className='flex justify-between mb-2'>
+            {/* title */}
+            <Link to={`/products/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+            {/* remove icon */}
+            <div onClick={()=> removeFromCart(id)} className='cursor-pointer text-xl'>
+              <IoMdClose className='text-gray-500 hover:text-red-500 cursor-pointer transiton' />
+            </div>
+          </div>
+          <div className='flex gap-x-2 h-[36px] text-sm'>
+            {/* qty */}
+            <div className='flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
+              {/* minis icon */}
+              <div onClick={()=> decreaseAmount(id)} className='flex-1 flex justify-center items-center cursor-pointer'>
+                <IoMdRemove />
+              </div>
+              {/* amount */}
+              <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+              {/* plus icon */}
+              <div onClick={()=> increaseAmount(id)} className='flex-1 h-full flex justify-center items-center cursor-pointer '>
+                <IoMdAdd />
+              </div>
+            </div>
+            {/* item price */}
+            <div className='flex-1 flex items-center justify-around'>$ { price }</div>
+            {/* final price */}
+            <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default CartItem;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const {cart} = useContext(CartContext);
+  const {cart, clearCart, total} = useContext(CartContext);
   return (<div className={`${
     isOpen ? 'right-0' : '-right-full'
     } w-full bg-white fixed top-0 h-full shadow-2x1 md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}>
@@ -19,9 +19,23 @@ const Sidebar = () => {
         <IoMdArrowForward className='text-2xl' />
       </div>
     </div>
-    <div>{cart.map((item) => {
-      return <CartItem>{item.title}</CartItem>
-    }) }</div>
+    <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border'>
+      {cart.map((item) => {
+      return <CartItem item={item} key={item.id} />
+      })}
+    </div>
+    <div className='flex flex-col gap-y-3 py-4 mt-4'>
+      <div className='flex w-full justify-between items-center'>
+        {/* total */}
+        <div className='uppercase font-semibold'>
+          <span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}
+        </div>
+        {/* clear cart icon */}
+        <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl'>
+          <FiTrash2 />
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
